1c7f84496e8994cc5ac40ece73d8bf1a74e700d7
Onur Can Öğünç <ogunc606@gmail.com>
2025-03-12 16:47:03 +0300

    Add AuthModule for Firebase authentication

---
+++ app/src/main/java/com/decode/mybooksummaries/di/AuthModule.kt
@@ -0,0 +1,44 @@
+package com.decode.mybooksummaries.di
+
+import com.decode.mybooksummaries.data.repository.AuthRepositoryImpl
+import com.decode.mybooksummaries.domain.repository.AuthRepository
+import com.decode.mybooksummaries.domain.usecase.AuthUseCases
+import com.decode.mybooksummaries.domain.usecase.auth.CreateAccountUseCase
+import com.decode.mybooksummaries.domain.usecase.auth.CurrentUserUseCase
+import com.decode.mybooksummaries.domain.usecase.auth.LoginUseCase
+import com.decode.mybooksummaries.domain.usecase.auth.LoginWithGoogleUseCase
+import com.decode.mybooksummaries.domain.usecase.auth.ResetPasswordUseCase
+import com.decode.mybooksummaries.domain.usecase.auth.SignOutUseCase
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthModule {
+
+    @Provides
+    @Singleton
+    fun providesFirebaseAuth() = Firebase.auth
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository = authRepositoryImpl
+
+    @Provides
+    @Singleton
+    fun provideAuthUseCases(
+        authRepository: AuthRepository
+    ) = AuthUseCases(
+        createAccountUseCase = CreateAccountUseCase(authRepository),
+        loginUseCase = LoginUseCase(authRepository),
+        loginWithGoogleUseCase = LoginWithGoogleUseCase(authRepository),
+        resetPasswordUseCase = ResetPasswordUseCase(authRepository),
+        signOutUseCase = SignOutUseCase(authRepository),
+        currentUserUseCase = CurrentUserUseCase(authRepository),
+    )
+}
