6d9ebbfb2e3afd75b9c5f9e89b50fe8bebd653aa
Onur Can Öğünç <ogunc606@gmail.com>
2025-03-12 13:17:46 +0300

    Updated themes,AndroidManifest and MainActivity for splash screen

--- app/src/main/java/com/decode/mybooksummaries/MainActivity.kt
+++ app/src/main/java/com/decode/mybooksummaries/MainActivity.kt
@@ -1,14 +1,55 @@
 package com.decode.mybooksummaries
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.decode.mybooksummaries.ui.theme.MyBookSummariesTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
+    var showSplashScreen = true
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                showSplashScreen
+            }
+            setOnExitAnimationListener { screen ->
+                val zoomX = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_X,
+                    0.5f,
+                    0f
+                )
+                val zoomY = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_Y,
+                    0.5f,
+                    0f
+                )
+                zoomX.duration = 500
+                zoomY.duration = 500
+                zoomX.interpolator = OvershootInterpolator()
+                zoomY.interpolator = OvershootInterpolator()
+                zoomX.doOnEnd {
+                    screen.remove()
+                }
+                zoomY.doOnEnd {
+                    screen.remove()
+                }
+                zoomY.start()
+                zoomX.start()
+            }
+        }
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
@@ -16,7 +57,10 @@ class MainActivity : ComponentActivity() {
 
             }
         }
-
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(3000)
+            showSplashScreen = false
+        }
     }
 }
 
