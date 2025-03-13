e6cf6e42c6ee83ee9fe2ee6d5976ed4f1ab0e0bb
Onur Can Öğünç <ogunc606@gmail.com>
2025-03-13 16:06:22 +0300

    Add WorkerModule

---
+++ app/src/main/java/com/decode/mybooksummaries/di/WorkerModule.kt
@@ -0,0 +1,26 @@
+package com.decode.mybooksummaries.di
+
+import android.content.Context
+import androidx.work.WorkManager
+import com.decode.mybooksummaries.data.workers.SyncWorkManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object WorkerModule {
+    @Provides
+    fun provideWorkManager(@ApplicationContext context: Context): WorkManager {
+        return WorkManager.getInstance(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSyncWorkManager(workManager: WorkManager): SyncWorkManager {
+        return SyncWorkManager(workManager)
+    }
+}
