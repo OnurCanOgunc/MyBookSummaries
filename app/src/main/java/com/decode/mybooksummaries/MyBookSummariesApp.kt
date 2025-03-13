64bf7e11c012fc42931450ee87a68b2a3a00e7c4
Onur Can Öğünç <ogunc606@gmail.com>
2025-03-13 16:11:42 +0300

    configure hilt workManager

--- app/src/main/java/com/decode/mybooksummaries/MyBookSummariesApp.kt
+++ app/src/main/java/com/decode/mybooksummaries/MyBookSummariesApp.kt
@@ -1,7 +1,18 @@
 package com.decode.mybooksummaries
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MyBookSummariesApp: Application()
+class MyBookSummariesApp: Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
