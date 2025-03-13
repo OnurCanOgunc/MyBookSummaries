7b2acfd7d7d3c32462cda9348edbf1a2a95efc4c
Onur Can Öğünç <ogunc606@gmail.com>
2025-03-13 11:31:58 +0300

    add Book data class

---
+++ app/src/main/java/com/decode/mybooksummaries/domain/model/Book.kt
@@ -0,0 +1,18 @@
+package com.decode.mybooksummaries.domain.model
+
+import com.google.firebase.Timestamp
+import java.util.UUID
+
+data class Book(
+    val id: String = UUID.randomUUID().toString(),
+    val title: String = "",
+    val author: String = "",
+    val pageCount: String = "0",
+    val currentPage: String = "0",
+    val genre: String = "Enter Genre",
+    val readingStatus: String = "To Read",
+    val startedReadingDate: Timestamp? = null,
+    val finishedReadingDate: Timestamp? = null,
+    val summary: String = "",
+    val imageUrl: String = ""
+)
