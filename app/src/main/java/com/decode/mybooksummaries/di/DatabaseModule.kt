5c1237cb79526e8da6d482c61ac27d020a923a81
Onur Can Öğünç <ogunc606@gmail.com>
2025-03-13 12:01:10 +0300

    add database module

---
+++ app/src/main/java/com/decode/mybooksummaries/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.decode.mybooksummaries.di
+
+import android.content.Context
+import androidx.room.Room
+import com.decode.mybooksummaries.data.local.dao.BookDao
+import com.decode.mybooksummaries.data.local.db.BookDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideBookDao(bookDatabase: BookDatabase): BookDao {
+        return bookDatabase.bookDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext appContext: Context): BookDatabase {
+        return Room.databaseBuilder(
+            appContext.applicationContext,
+            BookDatabase::class.java,
+            "books_database"
+        ).build()
+    }
+}
