76075975fa9069a2cd9a627ea83b012fc1ac3dfd
Onur Can Öğünç <ogunc606@gmail.com>
2025-03-20 17:05:20 +0300

    added new color and Lora font

--- app/src/main/java/com/decode/mybooksummaries/core/ui/theme/Color.kt
+++ app/src/main/java/com/decode/mybooksummaries/core/ui/theme/Color.kt
@@ -1,21 +1,89 @@
 package com.decode.mybooksummaries.core.ui.theme
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val deepBlue = Color(0xFF1E3A8A)
+val electricOrange =  Color(0xFF312F2F)
+val darElectricOrange = Color(0xFF3A3A3A)
+val softWhite = Color(0xFFF8FAFC)
+val darkSoftWhite = Color(0xFFD1D5DB)
+val coolGray = Color(0xFF737679)
+val darkCoolGray = Color(0xFF8D99AE)
+val charcoalBlack = Color(0xFF0F172A)
+val slateGray = Color(0xFF404952)
+val backgroundColor = Color(0xFFDED7C5)
+val darkBackgroundColor = Color(0xFF1E1E1E)
+val errorColor = Color(0xFFEF4444)
+val darkErrorColor = Color(0xFFFF6B6B)
+val textBlack = Color(0xFF232323)
+val darkTextBlack = Color(0xFFF8FAFC)
+val textWhite = Color(0xFFFFFFFF)
+val darkTextWhite = Color(0xFFB0B3B8)
+val deepRose = Color(0xFFC71585)
+val filterChipColor = Color(0xFF5A3E2B)
+val cinnamonRed = Color(0xFFB85C38)
+val darkFilterChipColor = Color(0xFF023047)
+val neonGreen = Color(0xFF04A777)
+val mutedGray = Color(0xFF6D6D6D)
+val onyxBlack = Color(0xFF121212)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
 
-val ErrorColor = Color(0xFFFF3333)
-val EmailButtonColor = Color(0xFFEEBA28)
-val BackgroundColor = Color(0xFFD5D5CF)
-val SearchBarContainerColor = Color(0xFF1F2937)
-val HomeBackgroundColor = Color(0xFF111827)
-val HomeBackgroundColor2 = Color(0xFFF8D8A2)
-val HomeTextColor = Color(0xFF989FAB)
-val LinearProgressIndicatorColor = Color(0xFF9CA3AF)
-val LinearProgressIndicatorTrackColor = Color(0xFF374151)
+val LocalLightColors = staticCompositionLocalOf { LightCustomColors }
+val LocalDarkColors = staticCompositionLocalOf { DarkCustomColors }
+
+@Immutable
+data class CustomColors(
+    val deepBlue: Color,
+    val electricOrange: Color,
+    val softWhite: Color,
+    val coolGray: Color,
+    val charcoalBlack: Color,
+    val slateGray: Color,
+    val backgroundColor: Color,
+    val errorColor: Color,
+    val textBlack: Color,
+    val textWhite: Color,
+    val deepRose: Color,
+    val textTitle: Color,
+    val textDesp: Color,
+    val filterChipColor: Color,
+    val selectedFilterChipColor: Color,
+)
+
+val LightCustomColors = CustomColors(
+    deepBlue = deepBlue,
+    electricOrange = electricOrange,
+    softWhite = softWhite,
+    coolGray = coolGray,
+    charcoalBlack = charcoalBlack,
+    slateGray = slateGray,
+    backgroundColor = backgroundColor,
+    errorColor = errorColor,
+    textBlack = textBlack,
+    textWhite = textWhite,
+    deepRose = deepRose,
+    textTitle = Color.Black,
+    textDesp = Color.Gray,
+    filterChipColor = filterChipColor,
+    selectedFilterChipColor = cinnamonRed
+)
+
+val DarkCustomColors = CustomColors(
+    deepBlue = deepBlue,
+    electricOrange = darElectricOrange,
+    softWhite = darkSoftWhite,
+    coolGray = darkCoolGray,
+    charcoalBlack = onyxBlack,
+    slateGray = mutedGray,
+    backgroundColor = darkBackgroundColor,
+    errorColor = darkErrorColor,
+    textBlack = darkTextBlack,
+    textWhite = darkTextWhite,
+    deepRose = deepRose,
+    textTitle = Color.White,
+    textDesp = textBlack,
+    filterChipColor = darkFilterChipColor,
+    selectedFilterChipColor = neonGreen
+)
--- app/src/main/java/com/decode/mybooksummaries/core/ui/theme/Type.kt
+++ app/src/main/java/com/decode/mybooksummaries/core/ui/theme/Type.kt
@@ -1,34 +1,134 @@
 package com.decode.mybooksummaries.core.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.decode.mybooksummaries.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
+val LocalTypography = staticCompositionLocalOf { LoraTypography }
+
+val loraFontFamily = FontFamily(
+    Font(R.font.lora_regular, FontWeight.Normal),
+    Font(R.font.lora_bold, FontWeight.Bold),
+    Font(R.font.lora_semi_bold, FontWeight.SemiBold),
+    Font(R.font.lora_medium, FontWeight.Medium)
+)
+
+@Immutable
+data class CustomTypography(
+    val displaySmall: TextStyle,
+    val headlineLarge: TextStyle,
+    val headlineMedium: TextStyle,
+    val headlineSmall: TextStyle,
+    val titleLarge: TextStyle,
+    val titleMedium: TextStyle,
+    val titleSmall: TextStyle,
+    val bodyExtraLarge: TextStyle,
+    val bodyLarge: TextStyle,
+    val bodyMedium: TextStyle,
+    val bodySmall: TextStyle,
+    val labelLarge: TextStyle,
+    val labelMedium: TextStyle,
+    val labelSmall: TextStyle,
+)
+
+val LoraTypography = CustomTypography(
+    displaySmall = TextStyle(
+        fontFamily = loraFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = loraFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = loraFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = loraFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+    ),
+    titleLarge = TextStyle(
+        fontFamily = loraFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = loraFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = loraFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    bodyExtraLarge = TextStyle(
+        fontFamily = loraFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 28.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = loraFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        letterSpacing = 0.4.sp
+    ),
+
+    bodyMedium = TextStyle(
+        fontFamily = loraFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = loraFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = loraFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = loraFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = loraFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
 )
