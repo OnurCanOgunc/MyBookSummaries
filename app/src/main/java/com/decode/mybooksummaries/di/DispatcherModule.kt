8f55d45e1fdfc048bd4834be814df431dd269495
Onur Can Öğünç <ogunc606@gmail.com>
2025-04-24 15:45:38 +0300

    Add DispatcherModule for coroutine dependency injection

---
+++ app/src/main/java/com/decode/mybooksummaries/di/DispatcherModule.kt
@@ -0,0 +1,34 @@
+package com.decode.mybooksummaries.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+
+    @Provides
+    @Singleton
+    @IoDispatcher
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @Singleton
+    @IoDispatcher
+    fun provideIoCoroutineScope(@IoDispatcher ioDispatcher: CoroutineDispatcher): CoroutineScope {
+        return CoroutineScope(SupervisorJob() + ioDispatcher)
+    }
+}
+
